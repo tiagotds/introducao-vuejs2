21ea037ad47f7a4340b2e1ad25dfd3846885a073
Tiago Donizete dos Santos <tiago.tds@gmail.com>
2018-03-11 23:18:04 -0300

    commit inicial

---
+++ netflixVue/src/componentes/teste.vue
@@ -0,0 +1,17 @@
+<template>
+	<h1>{{titulo}}</h1>
+
+</template>
+
+<script>
+	export default{
+		name: "teste",
+		props:['titulo']
+	}
+</script>
+
+<style lang="scss">
+	h1{
+		color: red;
+	}
+</style>
